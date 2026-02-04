2b0fb5a08591a9a65ddf4be51ae3bfc975caf911
Lukasz Stasienko <lukasz.stasienko@gmail.com>
2026-02-04 20:33:32 +0100

    login command

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 import {setUser, readConfig} from "./config"
+import {handlerLogin, registerCommand, runCommand, type CommandsRegistry } from "./commands"
+import console from "node:console";
+
 function main() {
-    setUser("Lukasz");
+    const cmdsRegistry: CommandsRegistry = {};
+    registerCommand(cmdsRegistry, "login", handlerLogin);
+    const args = process.argv.slice(2,);
+    if (args.length === 0) {
+        console.error("Command name expected.");
+        process.exit(1);
+    }
+    try {
+        runCommand(cmdsRegistry, args[0], ...args.slice(1,));
+    } catch  (err) {
+        console.error(err instanceof Error ? err.message : `Unknow error: ${err}`);
+        process.exit(1);
+    }
+
+
     const cfg = readConfig();
     console.log(cfg);
 }
